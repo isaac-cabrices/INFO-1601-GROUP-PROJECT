504bcc2521284d8e1a2faf94e13396339ab72115
isaac-cabrices <isaac.cabrices@gmail.com>
2021-04-09 16:39:57 -0400

    Added user input forms
    + Added forms page as AddTaskProject.html.
    + Added CSS to stop the side-nav from overlapping with main.
    + Changed project.html to link the existing pages together.

--- Habit Tracker Side-bar(Aaron 1st draft)/script.js
+++ Habit Tracker Side-bar(Aaron 1st draft)/script.js
@@ -1,4 +1,40 @@
 
+// User card data
+
+
+let tasks = [];
+//This is the object that holds all the user data. Example format:
+//  {
+//    id: Date.now(),
+//    name: "Example task",
+//    description: "This in an example task. Create 3 tasks.",
+//    todo: 3,
+//    completed: 0
+//  }
+
+
+// This function adds data to the tasks object, and then stores that object in local storage.
+const addTask = function(event){
+    event.preventDefault();
+    let task = { // gets the data from the form.
+        id: Date.now(),
+        name: document.getElementById('title').value,
+        description: document.getElementById('description').value,
+        todo: document.getElementById('todo').value,
+        completed: 0
+    }
+    tasks.push(task); // appends the form data to the object.
+    document.querySelector('form').reset(); //resets the fields.
+
+    localStorage.setItem('TaskList', JSON.stringify(tasks)); // stored locally. Stringify cause it has to be stored as a string.
+}
+
+// Listens for click in the form. Maybe it's better to have this with the forms html if it causes any errors.
+document.addEventListener('DOMContentLoaded', ()=> { //checks that the page is loaded first
+    document.getElementById('button').addEventListener('click', addTask); // button ID in the form
+});
+
+
 //One page navigation
 
 //Executes script for whatever page is called.
@@ -8,7 +44,7 @@ function executeScripts(){
     eval(script);
   }catch(oops){
     console.error(oops);
-  }   
+  }
 }
 
 //Puts content from url(or html file) into the main content container
@@ -19,7 +55,7 @@ async function loadContent(title,url) {
 
   if(url == null) {
     content.innerHTML = "";
-  } 
+  }
   else {
     let response = await fetch(url);
     content.innerHTML = await response.text();
@@ -51,7 +87,7 @@ function backPage(event){
     loadContent(null, null);
   }
   else{
-    loadContent(event.state.title, event.state.url);  
+    loadContent(event.state.title, event.state.url);
   }
 }
 
