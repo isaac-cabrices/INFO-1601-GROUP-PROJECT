316162aa33b26b99235fd68f5af097d9ad9d00cd
isaac-cabrices <isaac.cabrices@gmail.com>
2021-04-10 18:53:06 -0400

    delete data and fix of storage
    +Added storage.js API to fix storage
    + Added delete function
    + Fixed storage not working as intended after you switched pages.

--- Habit Tracker Side-bar(Aaron 1st draft)/script.js
+++ Habit Tracker Side-bar(Aaron 1st draft)/script.js
@@ -1,7 +1,7 @@
 
 // User data
 
-let tasks = [];
+const tasks = [];
 //This is the object that holds all the user data. Example format:
 //    id: Date.now(),
 //    name: "Example task",
@@ -10,8 +10,8 @@ let tasks = [];
 //    completed: 0
 
 // This function adds data to the tasks object, and then stores that object in local storage.
-const addTask = function(event){
-    event.preventDefault();
+const addToList = (event)=>{
+  event.preventDefault();
     let task = { // gets the data from the form.
         id: Date.now(),
         name: document.getElementById('title').value,
@@ -19,17 +19,40 @@ const addTask = function(event){
         todo: document.getElementById('todo').value,
         completed: 0
     }
-    tasks.push(task); // appends the form data to the object.
+    var oldTasks = store.get('tasklist');
+    oldTasks.push(task);
     document.querySelector('form').reset(); //resets the fields.
 
-    localStorage.setItem('TaskList', JSON.stringify(tasks)); // stored locally. Stringify cause it has to be stored as a string.
+    store.set('tasklist', oldTasks);
+}
+
+// when we print the tasks, their delete buttons should be matched with an index
+// (using a variable counter that matches with their index in the array)
+// so that when we call this removeTask function, it knows which one to remove.
+//eg. at counter 0:
+// <button onclick="removeTask(${tasks}, ${counter})" > delete task </a>
+
+//function removeTask(list, index){ // This is only for testing. pair up with below line
+const removeFromList = (event)=>{
+    event.preventDefault();
+    let index = prompt("INDEX TO REMOVE?"); // This is only for testing. Pair up with above line
+    var newTasks = store.get('tasklist');
+    if(index > newTasks.length || index < 0){
+      newTasks.splice(index, 1);
+      store.set('tasklist', newTasks);
+    }
+    else{
+      alert("Error! index not valid.");
+    }
 }
 
 // Listens for click in the form. Maybe it's better to have this with the forms html if it causes any errors.
 document.addEventListener('DOMContentLoaded', ()=> { //checks that the page is loaded first
-    document.getElementById('button').addEventListener('click', addTask); // button ID in the form
+    document.getElementById('button').addEventListener('click', addToList); // button ID in the form
+    //document.getElementById('deleteB').addEventListener('click', removeTask);
 });
 
+//------------------
 
 //One page navigation
 
