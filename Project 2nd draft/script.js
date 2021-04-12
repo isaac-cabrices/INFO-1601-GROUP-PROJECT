b22ede4b161bc47905d2c7ed00c2f96182f99cf0
isaac-cabrices <isaac.cabrices@gmail.com>
2021-04-12 19:23:01 -0400

    Patch 14 undefined storage bug
    + Fixed a bug where, if there was no object for storing habits in local storage already, it would not allow you to create tasks, since the location was undefined.

--- Project 2nd draft/script.js
+++ Project 2nd draft/script.js
@@ -97,29 +97,34 @@ submitButton.onclick = function() {
 //Get Info from form to create habit card
 let habitsList = [];
 const createNewHabit = (e)=>{
-    //prevent form from submitting as default
-    e.preventDefault();
+    if(document.getElementById('title').value != "" && document.getElementById('todo').value != "" ){
+        //prevent form from submitting as default
+        e.preventDefault();
 
-    //get form data into object
-    let habitInfo = {
-        id: Date.now(),
-        habitTitle: document.getElementById('title').value,
-        numRepeats: document.getElementById('todo').value,
-        description: document.getElementById('description').value
+        //get form data into object
+        let habitInfo = {
+            id: Date.now(),
+            habitTitle: document.getElementById('title').value,
+            numRepeats: document.getElementById('todo').value,
+            description: document.getElementById('description').value
+        }
+
+        //put form data object onto habits array. Appends it to existing list so as to not delete it.
+        var oldHabits = store.get('SavedHabits');
+        oldHabits.push(habitInfo);
+        console.log(oldHabits);
+
+        //reset form
+        document.getElementById('addTaskForm').reset();
+
+        //save to local storage
+        store.set('SavedHabits', oldHabits);
+
+        createHabitCard(habitInfo);
     }
-
-    //put form data object onto habits array
-    let oldHabits = store.get("SavedHabits");
-    oldHabits.push(habitInfo);
-    console.log(oldHabits);
-
-    //reset form
-    document.getElementById('addTaskForm').reset();
-
-    //save to local storage
-    storage.set('SavedHabits', oldHabits);
-
-    createHabitCard(habitInfo);
+    else{
+      alert("invalid card");
+    }
 }
 
 //Create habit card using form info from function above
@@ -155,6 +160,10 @@ document.addEventListener('DOMContentLoaded', ()=> {
 //When page reloaded, read local storage to get all habit cards
 function displayCardsOnReload(){
     let habitArray = store.get("SavedHabits");
+    if(habitArray == undefined){ // this if loop accounts for when there is no object created as yet.
+    store.set('SavedHabits', habitsList);
+      habitArray = store.get("SavedHabits");
+    }
     if(habitArray.length === 0){
         console.log("No Habits Stored");
     }
