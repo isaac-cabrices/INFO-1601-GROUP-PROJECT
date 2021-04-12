61f57518efecd17039208e77500eda2c79460351
isaac-cabrices <77852830+isaac-cabrices@users.noreply.github.com>
2021-04-12 18:44:47 -0400

    Patch 13 website update (#5)
    * Based
    
    * Based
    
    again, but correctly this time
    
    * Update
    
    + Fixed bug with storage where the habits were overwritten when you reloaded and made a new habit.
    
    * Patch 13 storage bug (#4)
    
    * Update habit tracker final.html
    
    Added IDs
    
    * Create script.js
    
    Script for deleting cards
    
    * Update habit tracker final.html
    
    Removed the IDs from the delete buttons. No longer needed.
    
    * Update script.js
    
    Fixed delete functionality
    
    * Update habit tracker final.html
    
    * Update script.js
    
    * Update script.js
    
    * Fixed storage bug
    
    with Store.js API
    
    Co-authored-by: Tristan Leid <77852358+crowbar72@users.noreply.github.com>
    Co-authored-by: AaronQuestel <73033823+AaronQuestel@users.noreply.github.com>
    
    Co-authored-by: Tristan Leid <77852358+crowbar72@users.noreply.github.com>
    Co-authored-by: AaronQuestel <73033823+AaronQuestel@users.noreply.github.com>

--- Project 2nd draft/script.js
+++ Project 2nd draft/script.js
@@ -1,3 +1,23 @@
+//Deleting cards
+
+function deleteCard(card) {
+
+  if (confirm('Are you sure you want to delete this card?')) {
+    document.getElementById(card.id).remove();
+  }
+
+}
+//Gets all the cards currently in the site by the .card class
+var cards;
+cards = document.querySelectorAll(".card");
+
+//For every card in the cards array sets an event listener on the delete button.
+for(let card of cards) {
+  card.querySelector(".delete-button").addEventListener('click', function() {
+    deleteCard(card);
+  });
+}
+
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -25,7 +45,7 @@ window.onclick = function(event) {
   }
 }
 
-
+/*
 const tasks = [];
 //This is the object that holds all the user data. Example format:
 //    id: Date.now(),
@@ -65,4 +85,106 @@ function removedata(index){
 
 document.addEventListener('DOMContentLoaded', ()=> { //checks that the page is loaded first
     document.getElementById('inputButton').addEventListener('click', addToList);
+});*/
+
+
+var submitButton = document.getElementById('inputButton');
+//when submit is pressed, move form
+submitButton.onclick = function() {
+    modal.style.display = "none";
+}
+
+//Get Info from form to create habit card
+let habitsList = [];
+const createNewHabit = (e)=>{
+    if(document.getElementById('title').value != "" && document.getElementById('todo').value != "" ){
+        //prevent form from submitting as default
+        e.preventDefault();
+
+        //get form data into object
+        let habitInfo = {
+            id: Date.now(),
+            habitTitle: document.getElementById('title').value,
+            numRepeats: document.getElementById('todo').value,
+            description: document.getElementById('description').value
+        }
+
+        //put form data object onto habits array. Appends it to existing list so as to not delete it.
+        var oldHabits = store.get('SavedHabits');
+        oldHabits.push(habitInfo);
+        console.log(oldHabits);
+
+        //reset form
+        document.getElementById('addTaskForm').reset();
+
+        //save to local storage
+        store.set('SavedHabits', oldHabits);
+
+        createHabitCard(habitInfo);
+    }
+    else{
+      alert("invalid card");
+    }
+}
+
+//Create habit card using form info from function above
+function createHabitCard(data){
+let habitArray = store.get("SavedHabits");
+let info = habitArray.find(function(habit, index){
+    if(habit.id == data.id)
+        return true;
 });
+let i = 0;
+let cardsPosition = document.querySelector('.cards-wrapper');
+let pbars = '';
+for(i = 0; i < info.numRepeats; i++){
+    pbars += '<div class="progress-bar"></div>'
+}
+cardsPosition.innerHTML += `
+    <div id="${info.id}" class="card">
+        <div class="habit-title">
+            <h1>${info.habitTitle}</h1>
+        </div>
+        <div id="delbut1" class="delete-button">X</div>
+        <div class="progress-bars">
+        ${pbars}
+        </div>
+    </div>
+        `
+}
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    submitButton.addEventListener('click', createNewHabit);
+});
+
+//When page reloaded, read local storage to get all habit cards
+function displayCardsOnReload(){
+    let habitArray = store.get("SavedHabits");
+    if(habitArray.length === 0){
+        console.log("No Habits Stored");
+    }
+    else{
+        for(let habit in habitArray){
+            habit = habitArray[habit];
+            console.log(habit);
+            let i = 0;
+            let cardsPosition = document.querySelector('.cards-wrapper');
+            let pbars = '';
+            for(i = 0; i < habit.numRepeats; i++){
+                pbars += '<div class="progress-bar"></div>'
+            }
+            cardsPosition.innerHTML += `
+                <div id="${habit.id}" class="card">
+                    <div class="habit-title">
+                        <h1>${habit.habitTitle}</h1>
+                    </div>
+                    <div id="delbut1" class="delete-button">X</div>
+                    <div class="progress-bars">
+                    ${pbars}
+                    </div>
+                </div>
+                `
+        }
+    }
+}
+window.onload = displayCardsOnReload;
